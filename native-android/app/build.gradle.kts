e8a2afd47d09a8ff35204c5bd039fdf16636d54e
admin <admin@litellm.local>
2026-08-14 08:20:25 +0800

    发布轻音 0.24

--- native-android/app/build.gradle.kts
+++ native-android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "im.molan.music"
         minSdk = 24
         targetSdk = 35
-        versionCode = 24
-        versionName = "1.0.23"
+        versionCode = 25
+        versionName = "0.24"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
