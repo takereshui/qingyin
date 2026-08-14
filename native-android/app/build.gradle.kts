6f88ba7a3744ecc985b3a89ee6db1cd03debd6b7
takereshui <1.22073102e+08+takereshui@users.noreply.github.com>
2026-08-14 03:32:32 +0000

    更正发布版本为 1.0.25

--- native-android/app/build.gradle.kts
+++ native-android/app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 26
-        versionName = "0.25"
+        versionName = "1.0.25"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
